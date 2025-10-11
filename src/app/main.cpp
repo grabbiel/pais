a97ee41188a5cb8b4172211e667737d418d01207
grabbiel <fcruzado22@gmail.com>
2025-10-11 16:07:28 -0700

    basic

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -13,34 +13,32 @@
 #include <atomic>
 
 int PIXEL_ENTRY(int argc, char **argv) {
-  int main() {
-    pixel::platform::WindowSpec ws;
-    ws.w = 1024;
-    ws.h = 576;
-    ws.title = "Pixel-Life (skeleton)";
-    auto r = pixel::renderer2d::Renderer::create(ws);
+  pixel::platform::WindowSpec ws;
+  ws.w = 1024;
+  ws.h = 576;
+  ws.title = "Pixel-Life (skeleton)";
+  auto r = pixel::renderer2d::Renderer::create(ws);
 
-    const double dt = 1.0 / 60.0;
-    double acc = 0.0, t0 = pixel::core::now_sec();
-    bool running = true;
-    while (running) {
-      SDL_Event e;
-      while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT)
-          running = false;
-      }
-      const double t1 = pixel::core::now_sec();
-      acc += (t1 - t0);
-      t0 = t1;
-
-      while (acc >= dt) { /* TODO logic tick */
-        acc -= dt;
-      }
-
-      r->begin_frame();
-      r->draw_demo();
-      r->end_frame();
+  const double dt = 1.0 / 60.0;
+  double acc = 0.0, t0 = pixel::core::now_sec();
+  bool running = true;
+  while (running) {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT)
+        running = false;
     }
-    return 0;
+    const double t1 = pixel::core::now_sec();
+    acc += (t1 - t0);
+    t0 = t1;
+
+    while (acc >= dt) { /* TODO logic tick */
+      acc -= dt;
+    }
+
+    r->begin_frame();
+    r->draw_demo();
+    r->end_frame();
   }
+  return 0;
 }
